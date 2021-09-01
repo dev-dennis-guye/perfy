fbe6f8a51dc6eb2e83720cd2d687f5029c3f44e1
Dennis <dennis@movesfinancial.com>
2021-08-31 22:54:06 -0400

    Wip

--- index.js
+++ index.js
@@ -1,184 +1,172 @@
-/* eslint no-param-reassign:0 */
-
-/**
- *  A simple, light-weight NodeJS utility for measuring code execution in high-resolution real times.
- *
- *  @module   perfy
- *  @author   Onur Yıldırım <onur@cutepilot.com>
- *  @license  MIT
- *  @example
- *      var perfy = require('perfy');
- *      perfy.start('loop-test');
- *      // some heavy stuff here...
- *      console.log(perfy.end('loop-test').time);
- */
-module.exports = (function () {
-
-    // See https://nodejs.org/api/process.html#process_process_hrtime
-
-    // --------------------------------
-    //  CLASS: PerfyItem
-    // --------------------------------
-
-    function PerfyItem(name) {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.perfy = void 0;
+var PerfyItem = /** @class */ (function () {
+    function PerfyItem(name, autoDestroy) {
+        if (autoDestroy === void 0) { autoDestroy = true; }
         this.name = name;
-        this.reset();
-    }
-
-    PerfyItem.prototype.reset = function () {
+        this.autoDestroy = autoDestroy;
         this.time = {
             start: null,
-            end: null
+            end: null,
         };
         this.utc = {
             start: null,
-            end: null
+            end: null,
+        };
+    }
+    PerfyItem.prototype.reset = function () {
+        this.time = {
+            start: null,
+            end: null,
+        };
+        this.utc = {
+            start: null,
+            end: null,
         };
         this.result = null;
     };
-
     PerfyItem.prototype.start = function () {
         this.reset();
         this.time.start = process.hrtime();
         this.utc.start = Date.now();
     };
-
     PerfyItem.prototype.end = function () {
+        var _a;
         if (!this.time.start) {
             throw new Error('start() should be called first!');
         }
         this.time.end = process.hrtime(this.time.start);
         this.utc.end = Date.now();
-
-        var o = {
-            name: this.name || '',
-            seconds: this.time.end[0],
-            nanoseconds: this.time.end[1],
-            // divide by a million to convert nanoseconds to milliseconds
-            milliseconds: (this.time.end[1] / 1000000),
-            startTime: this.utc.start,
-            endTime: this.utc.end
-        };
-        var fullMilliseconds = (o.seconds * 1000) + o.milliseconds;
-        o.fullMilliseconds = Number(fullMilliseconds.toFixed(3));
-        o.fullSeconds = o.time = Number((fullMilliseconds / 1000).toFixed(3));
-        o.fullNanoseconds = (o.seconds * 1000 * 1000000) + o.nanoseconds;
-
+        var name = (_a = this.name) !== null && _a !== void 0 ? _a : '';
+        var seconds = this.time.end[0];
+        var nanoseconds = this.time.end[1];
+        // divide by a million to convert nanoseconds to milliseconds
+        var milliseconds = this.time.end[1] / 1000000;
+        var startTime = this.utc.start;
+        var endTime = this.utc.end;
+        var fullMilliseconds = Number((seconds * 1000 + milliseconds).toFixed(3));
+        var time = Number((fullMilliseconds / 1000).toFixed(3));
+        var fullSeconds = time;
+        var fullNanoseconds = seconds * 1000 * 1000000 + nanoseconds;
         var n = this.name ? this.name + ': ' : '';
-        o.summary = n + o.time + ' sec.';
-        this.result = o;
-        return o;
-    };
-
-    // --------------------------------
-    //  CLASS: perfy
-    // --------------------------------
-
-    // storage for PerfyItem instances
-    var perfList = {},
-        perfy = {},
-        ERR = {
-            NAME: 'Performance instance name required!',
-            NOITEM: 'No performance instance with name: ',
-            CALLBACK: 'Callback is not a function!'
+        var summary = n + time + ' sec.';
+        this.result = {
+            name: name,
+            seconds: seconds,
+            nanoseconds: nanoseconds,
+            milliseconds: milliseconds,
+            startTime: startTime,
+            endTime: endTime,
+            fullMilliseconds: fullMilliseconds,
+            time: time,
+            fullNanoseconds: fullNanoseconds,
+            fullSeconds: fullSeconds,
+            summary: summary,
         };
-
-    perfy.start = function (name, autoDestroy) {
-        if (!name) { throw new Error(ERR.NAME); }
-        name = String(name);
-        autoDestroy = typeof autoDestroy === 'undefined' ? true : autoDestroy;
-        perfList[name] = new PerfyItem(name);
-        perfList[name].autoDestroy = autoDestroy;
-        perfList[name].start();
-        return perfy;
+        return this.result;
     };
-
-    perfy.end = function (name) {
-        if (!name) { throw new Error(ERR.NAME); }
-        name = String(name);
-        var p = perfList[name];
-        if (!p) { throw new Error(ERR.NOITEM + name); }
+    return PerfyItem;
+}());
+var ErrCodes;
+(function (ErrCodes) {
+    ErrCodes["NAME"] = "Performance instance name required!";
+    ErrCodes["NO_ITEM"] = "No performance instance with name: ";
+    ErrCodes["CALLBACK"] = "Callback is not a function!";
+})(ErrCodes || (ErrCodes = {}));
+var Perfy = /** @class */ (function () {
+    function Perfy() {
+        this.perfList = {};
+    }
+    Perfy.prototype.start = function (name, autoDestroy) {
+        if (autoDestroy === void 0) { autoDestroy = true; }
+        if (!name) {
+            throw new Error(ErrCodes.NAME);
+        }
+        this.perfList[name] = new PerfyItem(name, autoDestroy);
+        this.perfList[name].start();
+        return this;
+    };
+    Perfy.prototype.end = function (name) {
+        if (!name) {
+            throw new Error(ErrCodes.NAME);
+        }
+        var p = this.perfList[name];
+        if (!p) {
+            throw new Error(ErrCodes.NO_ITEM + ": " + name);
+        }
         // if already ended and has result, return
-        if (p.result) { return p.result; }
+        if (p.result) {
+            return p.result;
+        }
         var result = p.end();
         if (p.autoDestroy) {
-            delete perfList[name];
+            delete this.perfList[name];
         }
         return result;
     };
-
-    perfy.result = function (name) {
-        if (!name) { throw new Error(ERR.NAME); }
-        name = String(name);
-        var p = perfList[name];
-        if (!p) { return null; }
+    Perfy.prototype.result = function (name) {
+        if (!name) {
+            throw new Error(ErrCodes.NAME);
+        }
+        var p = this.perfList[name];
+        if (!p || !p.result) {
+            return null;
+        }
         return p.result;
     };
-
-    perfy.exists = function (name) {
-        if (!name) { throw new Error(ERR.NAME); }
-        return Boolean(perfList[name]);
-    };
-
-    perfy.names = function () {
-        return Object.keys(perfList);
-    };
-
-    perfy.count = function () {
-        return perfy.names().length;
-    };
-
-    perfy.destroy = function (name) {
-        if (!name) { throw new Error(ERR.NAME); }
-        if (perfList[name]) {
-            delete perfList[name];
+    Perfy.prototype.exists = function (name) {
+        if (!name) {
+            throw new Error(ErrCodes.NAME);
         }
-        return perfy;
+        return !!this.perfList[name];
     };
-
-    perfy.destroyAll = function () {
-        perfList = {};
-        return perfy;
+    Perfy.prototype.names = function () {
+        return Object.keys(this.perfList);
     };
-
-    perfy.exec = function (name, fn) {
-        if (typeof fn !== 'function') {
-            if (typeof name === 'function') {
-                fn = name;
-                name = null;
-            } else {
-                throw new Error(ERR.CALLBACK);
-            }
+    Perfy.prototype.count = function () {
+        return this.names().length;
+    };
+    Perfy.prototype.destroy = function (name) {
+        if (!name) {
+            throw new Error(ErrCodes.NAME);
         }
-
+        if (this.perfList[name]) {
+            delete this.perfList[name];
+        }
+        return this;
+    };
+    Perfy.prototype.destroyAll = function () {
+        this.perfList = {};
+        return this;
+    };
+    Perfy.prototype.exec = function (_a) {
+        var _this = this;
+        var fn = _a.fn, _b = _a.name, name = _b === void 0 ? '' : _b;
         var p;
         if (name) {
-            perfList[name] = new PerfyItem(name);
-            perfList[name].autoDestroy = false;
-            p = perfList[name];
-        } else {
-            p = new PerfyItem();
+            this.perfList[name] = new PerfyItem(name, true);
+            p = this.perfList[name];
         }
-        function done() {
+        else {
+            p = new PerfyItem('');
+        }
+        var done = function () {
             var result = p.end();
             if (name && p.autoDestroy) {
-                delete perfList[name];
+                delete _this.perfList[name];
             }
             return result;
-        }
+        };
         p.start();
         if (fn.length > 0) {
             fn(done);
-            return perfy;
+            return this;
         }
         fn();
         return done();
     };
-
-    // --------------------------------
-    //  EXPORT
-    // --------------------------------
-
-    return perfy;
-
+    Perfy.instance = new Perfy();
+    return Perfy;
 }());
+exports.perfy = new Perfy();
